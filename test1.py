bdfbfcc826be79d4cdf72dfe0542328171a032ca
je223xa <stud15859@fh-wedel.de>
2023-06-18 10:32:52 +0200

    small changes

--- test1.py
+++ test1.py
@@ -1,50 +1,48 @@
 
 import pandas as pd
-# import tensorflow as tf
-FILE = '/Users/jonny/Documents/capture52_1.csv'
-FILE2 = "/Users/jonny/Downloads/opt/Malware-Project/BigDataset/IoTScenarios/CTU-IoT-Malware-Capture-52-1/bro/conn.log.labeled"
-SHUFFLE_BUFFER = 500
-BATCH_SIZE = 2
+import socket
+import struct
+import numpy as np
 
 
-def convert_to_csv():
-    fieldsIN = ['ts', 'uid', 'id.orig_h', 'id.orig_p', 'id.resp_h', 'id.resp_p', 'proto', 'service', 'duration', 'orig_bytes', 'resp_bytes', 'conn_state',
-              'local_orig', 'local_resp', 'missed_bytes', 'history', 'orig_pkts', 'orig_ip_bytes', 'resp_pkts', 'resp_ip_bytes', 'tunnel_parents', 'label', 'detailed-label']
-    fieldsOUT = ['ts', 'uid', 'id.orig_h', 'id.orig_p', 'id.resp_h', 'id.resp_p', 'proto', 'service', 'duration', 'orig_bytes', 'resp_bytes', 'conn_state',
-              'local_orig', 'local_resp', 'missed_bytes', 'history', 'orig_pkts', 'orig_ip_bytes', 'resp_pkts', 'resp_ip_bytes', 'label']
-    dtypes = {"ts": "int", "uid": "string", "id.orig_h": "string", "id.orig_p": "int", "id.resp_h": "string", "id.resp_p": "int", 
-              "proto": "string", "service": "string", "duration": "string", "orig_bytes": "float", "resp_bytes": "int", "conn_state": "string",
-              "local_orig": "string", "local_resp": "string", "missed_bytes": "int", "historyw": "string", "orig_pkts": "int", "orig_ip_bytes": "int", "resp_pkts": "int", "resp_ip_bytes": "int", "label": "string"}
-    df = pd.read_csv(FILE2, sep='\x09', skiprows=10, skipfooter=2,
-                     names=fieldsIN, header=None, engine='python', dtype=dtypes)
-    df.to_csv('capture52_1.csv', columns=fieldsOUT)
+def convert_ipv4(addr):
+    return struct.unpack("!I", socket.inet_aton(addr))[0]
 
+def con_proto(proto):
+    protos = ['icmp', 'tcp', 'udp']
+    return protos.index(proto)
 
-def pandatry():
-    dtypes = {"ts": "float", "uid": "string", "id.orig_h": "string", "id.orig_p": "int", "id.resp_h": "string", "id.resp_p": "int", 
-              "proto": "string", "service": "string", "duration": "string", "orig_bytes": "float", "resp_bytes": "int", "conn_state": "string",
-              "local_orig": "string", "local_resp": "string", "missed_bytes": "int", "history": "string", "orig_pkts": "int", "orig_ip_bytes": "int", "resp_pkts": "int", "resp_ip_bytes": "int", "tunnel_parents": "string", "label": "string"}
-    df = pd.read_csv(FILE, encoding='big5hkscs', dtype=dtypes)
-    # numeric_feature_names = ['age', 'thalach', 'trestbps',  'chol', 'oldpeak']
-    # numeric_features = df[numeric_feature_names]
-    # numeric_features.head()
-    print(df.head())
+def norm_df(df, split_into=None):
+    """
+    param: split_into [this is a percentage, how much of the dataset should be used]
+    """
+    assert 0 < split_into < 1, "Number must be percentage"
+    df = df.replace('-', np.nan)
+    df['orig_h'] = df['orig_h'].apply(convert_ipv4)
+    df['resp_h'] = df['resp_h'].apply(convert_ipv4)
+    # df = pd.get_dummies(df, columns=['proto'], dtype=int)
+    df['proto'] = df['proto'].apply(con_proto)
+    # print(dummies.head())
+    df = df.drop(['Unnamed: 0','service', 'duration', 'missed_bytes', 'history', 'uid', 'conn_state', 'local_orig', 'local_resp', 'orig_ip_bytes', 'resp_ip_bytes', 'orig_pkts', 'resp_pkts', 'detailed_label', 'ts'], axis=1)
+    df['label'] = (df['label'] == "Malicious").astype(int)
+    # df['resp_bytes'] = df['resp_bytes'].apply(to_int)
+    df = df.dropna()
+    df['resp_bytes'] = df['resp_bytes'].astype(int)
+    df['orig_bytes'] = df['orig_bytes'].astype(int)
+    if (split_into): df = np.array_split(df.sample(frac=1), 1/split_into)[0]
+    df = df.reset_index(drop=True)
+    return df
 
+def convert_to_csv(FILEID):
+    NUMERIC_COLUMNS = ['ts', 'orig_p', 'resp_p', "orig_bytes", "resp_bytes", "missed_bytes", "orig_pkts", 'orig_ip_bytes', 'resp_pkts', 'resp_ip_bytes', 'duration'] # integer columns
+    LOGFILE = f"/Users/jonny/Downloads/opt/Malware-Project/BigDataset/IoTScenarios/CTU-IoT-Malware-Capture-{FILEID}-1/bro/conn.log.labeled"
+    fieldsIN = ['ts', 'uid', 'orig_h', 'orig_p', 'resp_h', 'resp_p', 'proto', 'service', 'duration', 'orig_bytes', 'resp_bytes', 'conn_state',
+            'local_orig', 'local_resp', 'missed_bytes', 'history', 'orig_pkts', 'orig_ip_bytes', 'resp_pkts', 'resp_ip_bytes', 'tunnel_parents', 'label', 'detailed_label']
+    df = pd.read_csv(LOGFILE, sep="\x09|\x20\x20\x20", skiprows=10, skipfooter=2,
+                    names=fieldsIN, header=None, engine='python')
+                            
+    df = df.drop(['tunnel_parents'], axis=1)
+    df.to_csv(f'csv/capture{FILEID}_1.csv')
+    return df
 
-def generate_dtypes():
-    fields = ['ts', 'uid', 'id.orig_h', 'id.orig_p', 'id.resp_h', 'id.resp_p', 'proto', 'service', 'duration', 'orig_bytes', 'resp_bytes', 'conn_state',
-              'local_orig', 'local_resp', 'missed_bytes', 'history', 'orig_pkts', 'orig_ip_bytes', 'resp_pkts', 'resp_ip_bytes', 'tunnel_parents', 'label', 'detailed-label']
-    types = ['time', 'string', 'addr',
-             'port', 'addr', 'port', 'enum', 'string', 'interval', 'count', 'count', 'string', 'bool', 'bool', 'count', 'string', 'count', 'count', 'count', 'count', 'string', 'string']
-    print("{")
-    for ft in zip(fields, types):
-        print(f"\"{ft[0]}\":\"{ft[1]}\",")
-    print("}")
-
-
-def main():
-    convert_to_csv()
-
-
-if __name__ == '__main__':
-    main()
+convert_to_csv(1)
