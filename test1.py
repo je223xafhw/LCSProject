9eaf40b4d878959b7004c4718e6050baa3f0e432
je223xafhw <stud105859@fh-wedel.de>
2023-06-19 16:05:42 +0200

    a commit

--- test1.py
+++ test1.py
@@ -1,50 +1,26 @@
 
 import pandas as pd
-# import tensorflow as tf
-FILE = '/Users/jonny/Documents/capture52_1.csv'
-FILE2 = "/Users/jonny/Downloads/opt/Malware-Project/BigDataset/IoTScenarios/CTU-IoT-Malware-Capture-52-1/bro/conn.log.labeled"
-SHUFFLE_BUFFER = 500
-BATCH_SIZE = 2
+
+def fetch_log(FILEID):
+    LOGFILE = f"https://mcfp.felk.cvut.cz/publicDatasets/IoTDatasets/CTU-IoT-Malware-Capture-{FILEID}-1/bro/conn.log.labeled"
+    # LOGFILE = f"/Users/jonny/Downloads/opt/Malware-Project/BigDataset/IoTScenarios/CTU-IoT-Malware-Capture-{FILEID}-1/bro/conn.log.labeled"
+    fieldsIN = ['ts', 'uid', 'orig_h', 'orig_p', 'resp_h', 'resp_p', 'proto', 'service', 'duration', 'orig_bytes', 'resp_bytes', 'conn_state',
+            'local_orig', 'local_resp', 'missed_bytes', 'history', 'orig_pkts', 'orig_ip_bytes', 'resp_pkts', 'resp_ip_bytes', 'tunnel_parents', 'label', 'detailed_label']
+    return pd.read_csv(LOGFILE, sep="\x09|\x20\x20\x20", skiprows=10, skipfooter=2,
+                    names=fieldsIN, header=None, engine='python')
 
 
-def convert_to_csv():
-    fieldsIN = ['ts', 'uid', 'id.orig_h', 'id.orig_p', 'id.resp_h', 'id.resp_p', 'proto', 'service', 'duration', 'orig_bytes', 'resp_bytes', 'conn_state',
-              'local_orig', 'local_resp', 'missed_bytes', 'history', 'orig_pkts', 'orig_ip_bytes', 'resp_pkts', 'resp_ip_bytes', 'tunnel_parents', 'label', 'detailed-label']
-    fieldsOUT = ['ts', 'uid', 'id.orig_h', 'id.orig_p', 'id.resp_h', 'id.resp_p', 'proto', 'service', 'duration', 'orig_bytes', 'resp_bytes', 'conn_state',
-              'local_orig', 'local_resp', 'missed_bytes', 'history', 'orig_pkts', 'orig_ip_bytes', 'resp_pkts', 'resp_ip_bytes', 'label']
-    dtypes = {"ts": "int", "uid": "string", "id.orig_h": "string", "id.orig_p": "int", "id.resp_h": "string", "id.resp_p": "int", 
-              "proto": "string", "service": "string", "duration": "string", "orig_bytes": "float", "resp_bytes": "int", "conn_state": "string",
-              "local_orig": "string", "local_resp": "string", "missed_bytes": "int", "historyw": "string", "orig_pkts": "int", "orig_ip_bytes": "int", "resp_pkts": "int", "resp_ip_bytes": "int", "label": "string"}
-    df = pd.read_csv(FILE2, sep='\x09', skiprows=10, skipfooter=2,
-                     names=fieldsIN, header=None, engine='python', dtype=dtypes)
-    df.to_csv('capture52_1.csv', columns=fieldsOUT)
+def test_dfs():
+    ids = [1,3,7,8,9,17,20,21,33, 34,35,36,39, 42,43,44,48,49,52,60]
+    for id in ids:
+        try:
+            df = fetch_log(id)
+            print(f"The {id} dataset has shape {df.shape} with {len(df[df['label'] == 'Malicious'])} malicious and {len(df[df['label'] == 'Benign'])} benign")
+        except Exception as e:
+            print(f"id {id} not working: {e}")
+        df = None
 
 
-def pandatry():
-    dtypes = {"ts": "float", "uid": "string", "id.orig_h": "string", "id.orig_p": "int", "id.resp_h": "string", "id.resp_p": "int", 
-              "proto": "string", "service": "string", "duration": "string", "orig_bytes": "float", "resp_bytes": "int", "conn_state": "string",
-              "local_orig": "string", "local_resp": "string", "missed_bytes": "int", "history": "string", "orig_pkts": "int", "orig_ip_bytes": "int", "resp_pkts": "int", "resp_ip_bytes": "int", "tunnel_parents": "string", "label": "string"}
-    df = pd.read_csv(FILE, encoding='big5hkscs', dtype=dtypes)
-    # numeric_feature_names = ['age', 'thalach', 'trestbps',  'chol', 'oldpeak']
-    # numeric_features = df[numeric_feature_names]
-    # numeric_features.head()
-    print(df.head())
-
-
-def generate_dtypes():
-    fields = ['ts', 'uid', 'id.orig_h', 'id.orig_p', 'id.resp_h', 'id.resp_p', 'proto', 'service', 'duration', 'orig_bytes', 'resp_bytes', 'conn_state',
-              'local_orig', 'local_resp', 'missed_bytes', 'history', 'orig_pkts', 'orig_ip_bytes', 'resp_pkts', 'resp_ip_bytes', 'tunnel_parents', 'label', 'detailed-label']
-    types = ['time', 'string', 'addr',
-             'port', 'addr', 'port', 'enum', 'string', 'interval', 'count', 'count', 'string', 'bool', 'bool', 'count', 'string', 'count', 'count', 'count', 'count', 'string', 'string']
-    print("{")
-    for ft in zip(fields, types):
-        print(f"\"{ft[0]}\":\"{ft[1]}\",")
-    print("}")
-
-
-def main():
-    convert_to_csv()
-
-
-if __name__ == '__main__':
-    main()
+        
+        
+test_dfs()
